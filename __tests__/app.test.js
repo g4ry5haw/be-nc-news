f0599e1b792ee02d05f0cf131ae24824a20dd325
Gary Shaw <113700662+g4ry5haw@users.noreply.github.com>
2022-11-15 12:08:41 +0000

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,7 +56,7 @@ describe("GET /api/articles", () => {
             topic: expect.any(String),
             created_at: expect.any(String),
             votes: expect.any(Number),
-            comment_count: expect.any(String),
+            comment_count: expect.any(Number),
           });
         });
       });
