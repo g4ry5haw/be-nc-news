3357f22844a336fe6ec156faab1381ad4d2f8363
Gary Shaw <113700662+g4ry5haw@users.noreply.github.com>
2022-11-15 12:08:06 +0000

    Update news.model.js

--- models/news.model.js
+++ models/news.model.js
@@ -16,7 +16,7 @@ exports.selectArticles = () => {
   return db
     .query(
       `
-      SELECT articles.*, COUNT(comment_id) AS comment_count
+      SELECT articles.*, COUNT(comment_id)::INT AS comment_count
       FROM articles
       LEFT JOIN comments ON articles.article_id = comments.article_id
       GROUP BY articles.article_id
