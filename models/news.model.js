c80d33f144a42ab8436524ea5bef1966d6d2ac22
Gary Shaw <garyshaw05@gmail.com>
2022-11-18 16:23:09 +0000

    task 11 removed console.logs

--- models/news.model.js
+++ models/news.model.js
@@ -59,8 +59,6 @@ exports.selectArticles = (topic, sort_by = "created_at", order = "desc") => {
 
 exports.selectArticleById = (article_id) => {
   return checkArticleExists(article_id).then(() => {
-    console.log("model");
-    console.log(article_id);
     return db
       .query(
         `
@@ -73,7 +71,6 @@ exports.selectArticleById = (article_id) => {
         [article_id]
       )
       .then((result) => {
-        console.log(result.rows);
         return result.rows;
       });
   });
@@ -81,10 +78,10 @@ exports.selectArticleById = (article_id) => {
 
 // exports.selectArticleByArticleId = async (articleId) => {
 //   const res = await db.query(
-//     `SELECT articles.*, COUNT(comment_id)::int AS comment_count 
-//     FROM articles 
-//     LEFT JOIN comments ON comments.article_id = articles.article_id 
-//     WHERE articles.article_id = $1 
+//     `SELECT articles.*, COUNT(comment_id)::int AS comment_count
+//     FROM articles
+//     LEFT JOIN comments ON comments.article_id = articles.article_id
+//     WHERE articles.article_id = $1
 //     GROUP BY articles.article_id;`,
 //     [articleId]
 //   );
